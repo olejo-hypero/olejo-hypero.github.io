ff1a6d16f0e80116b9ff8a94dc03357a730c7ca9
olejo-hypero <programpsc@gmail.com>
2019-10-26 00:50:24 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,6 +22,13 @@ $(document).ready(function(){
         }
         return false; // выключаем стандартное действие
     });
+	$('.footer-menu__link').click( function(){ // ловим клик по ссылке с классом go_to
+        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top-155 }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+        return false; // выключаем стандартное действие
+    });
     $('.tabs').each(function(i) {
         var storage = localStorage.getItem('tab' + i);
         if (storage) {
