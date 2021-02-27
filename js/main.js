326dd9b93592bb4350661931ee4acb39bdc521d5
olejo-hypero <programpsc@gmail.com>
2021-02-28 01:27:54 +0500

    dd
    dd

--- js/main.js
+++ js/main.js
@@ -4,13 +4,10 @@ $(document).ready(function(){
   background.initializr = function (){
     
     var $this = this;
-     
-
-   
     //option
     $this.id = "about";
-    $this.style = {bubbles_color:"#fff",stroke_width:0, stroke_color :"black"};
-    $this.bubbles_number = 30;
+    $this.style = {bubbles_color:"#fedb37",stroke_width:0, stroke_color :"black"};
+    $this.bubbles_number = 10;
     $this.speed = [1500,8000]; //milliseconds
     $this.max_bubbles_height = $this.height;
     $this.shape = false // 1 : circle | 2 : triangle | 3 : rect | false :random
@@ -25,28 +22,20 @@ $(document).ready(function(){
     $this.width = $this.object.width($this.ww);
     $this.height = $this.object.height($this.wh);
     
-    
     $("body").prepend("<style>.shape_background {transform-origin:center; width:80px; height:80px; background: "+$this.style.bubbles_color+"; position: absolute}</style>");
-    
     
     for (i = 0; i < $this.bubbles_number; i++) {
         $this.generate_bubbles()
     }
-    
   }
-
-  
-  
-  
-
    background.generate_bubbles = function() {
      var $this = this;
      var base = $("<div class='shape_background'></div>");
-     var shape_type = $this.shape ? $this.shape : Math.floor($this.rn(1,3));
+     var shape_type = $this.shape ? $this.shape : Math.floor($this.rn(1,1.2));
      if(shape_type == 1) {
-       var bolla = base.css({borderRadius: "50%"})
+       var bolla = base.css({borderRadius: "10%"})
      }else if (shape_type == 2){
-       var bolla = base.css({width:0, height:0, "border-style":"solid","border-width":"0 40px 69.3px 40px","border-color":"transparent transparent "+$this.style.bubbles_color+" transparent", background:"transparent"}); 
+       var bolla = base.css({width:0, height:0, borderRadius: "10%", "border-style":"solid","border-width":"0 20px 39.3px 20px","border-color":"transparent transparent "+$this.style.bubbles_color+" transparent", background:"transparent"}); 
      }else{
        var bolla = base; 
      }    
@@ -57,9 +46,7 @@ $(document).ready(function(){
        $(this).remove();
        $this.generate_bubbles();
      })
-       
     }
-
 
 background.rn = function(from, to, arr) {
   if(arr){
