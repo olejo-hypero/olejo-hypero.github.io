6c618318808b616362c6ba492460537dd8726eb5
Oleg <programpsc@gmail.com>
2023-04-02 18:29:21 +0500

    1
    1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+  [].forEach.call(document.querySelectorAll("img[data-src]"), function (img) {
+    img.setAttribute("src", img.getAttribute("data-src"));
+    img.onload = function () {
+      img.removeAttribute("data-src");
+    };
+  });
   $(".main-logo-description span").typed({
     strings: [
       "Разработка сайтов",
